a5835f3e7b11fe88d3b4f9f7a93d05ee717d6db0
muzibai <821239448@qq.com>
2018-11-28 14:02:38 +0800

    register test

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, EqualTo
 from flask_migrate import Migrate, MigrateCommand
 import os
 
@@ -42,6 +42,17 @@ class LoginForm(Form):
     submit = SubmitField(u"登入")
 
 
+class RegistrationForm(Form):
+    username = StringField(u"用户名", validators=[DataRequired()])
+    password = PasswordField(u"密码", validators=[DataRequired(), EqualTo('password2', message=u"两次密码必须一样")])
+    password2 = PasswordField(u"密码确认", validators=[DataRequired()])
+    submit = SubmitField(u"注册")
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(u"用户名正在被使用")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -52,6 +63,17 @@ def login():
             if password == user.password:
                 return render_template('login.html')
     return render_template('index.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
